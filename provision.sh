81076bdc42095b84ba1f9c8254a1aac930918714
Andy French <andy@dissimulator.com>
2015-08-11 23:30:12 +0100

    Added upstart script to make sure Consul starts when a box starts up subsequent to the initial provisioning.

--- provision.sh
+++ provision.sh
@@ -4,18 +4,21 @@
 apt-get update
 apt-get install -y unzip
 
-# Step 2 - Get the Consul Zip file and extract it.  
+# Step 2 - Copy the upstart script to the /etc/init folder.
+cp /vagrant/consul.conf /etc/init/consul.conf
+
+# Step 3 - Get the Consul Zip file and extract it.  
 cd /usr/local/bin
 wget https://dl.bintray.com/mitchellh/consul/0.5.2_linux_amd64.zip
 unzip *.zip
 rm *.zip
 
-# Step 3 - Make the Consul directory.
+# Step 4 - Make the Consul directory.
 mkdir -p /etc/consul.d
 mkdir /var/consul
 
-# Step 4 - Copy the server configuration.
+# Step 5 - Copy the server configuration.
 cp $1 /etc/consul.d/config.json
 
-# Step 5 - Start Consul
-consul agent -config-file=/etc/consul.d/config.json
+# Step 6 - Start Consul
+exec consul agent -config-file=/etc/consul.d/config.json
--- provisionclient.sh
+++ provisionclient.sh
@@ -4,23 +4,26 @@
 apt-get update
 apt-get install -y unzip
 
-# Step 2 - Get the Consul Zip file and extract it.  
+# Step 2 - Copy the upstart script to the /etc/init folder.
+cp /vagrant/consul.conf /etc/init/consul.conf
+
+# Step 3 - Get the Consul Zip file and extract it.  
 cd /usr/local/bin
 wget https://dl.bintray.com/mitchellh/consul/0.5.2_linux_amd64.zip
 unzip *.zip
 rm *.zip
 
-# step 3 - Get the Consul UI
+# step 4 - Get the Consul UI
 wget https://dl.bintray.com/mitchellh/consul/0.5.2_web_ui.zip
 unzip *.zip
 rm *.zip
 
-# Step 4 - Make the Consul directory.
+# Step 5 - Make the Consul directory.
 mkdir -p /etc/consul.d
 mkdir /var/consul
 
-# Step 5 - Copy the server configuration.
+# Step 6 - Copy the server configuration.
 cp $1 /etc/consul.d/config.json
 
-# Step 6 - Start Consul
-consul agent -config-file=/etc/consul.d/config.json
+# Step 7 - Start Consul
+exec consul agent -config-file=/etc/consul.d/config.json
